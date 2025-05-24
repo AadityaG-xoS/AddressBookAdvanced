34245c0f6a2ceb50bbc7dfbea6c46528e3209b97
AadityaGawali <97809690+AadityaGawali@users.noreply.github.com>
2025-05-25 01:15:44 +0530

    UC7: Applied Lombok annotations to AddressBookData entity

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -1,17 +1,17 @@
 package com.bridgelabz.addressbookapp.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "address_book")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class AddressBookData {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String name;
@@ -20,4 +20,14 @@ public class AddressBookData {
     private String state;
     private String zip;
     private String phoneNumber;
+
+    // Constructor for mapping DTO to Entity
+    public AddressBookData(AddressBookDTO dto) {
+        this.name = dto.getName();
+        this.address = dto.getAddress();
+        this.city = dto.getCity();
+        this.state = dto.getState();
+        this.zip = dto.getZip();
+        this.phoneNumber = dto.getPhoneNumber();
+    }
 }
